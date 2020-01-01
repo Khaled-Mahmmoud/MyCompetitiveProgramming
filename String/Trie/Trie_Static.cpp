681c6a3676e01f7f4c687f158c8159b5f34539d7
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-01 09:43:38 -0800

    Update and rename Code_of_Trie.cpp to Trie_Static.cpp

--- String/Trie/Trie_Static.cpp
+++ String/Trie/Trie_Static.cpp
@@ -1,40 +1,59 @@
-// for implementation O(N * L) Where n is number of words and L is maximum string length OR average
+// for implementation O(N * L) Where n is number of words and L is average length of strings
 // for insert OR search O(L) Where L length of a word
 #include <bits/stdc++.h>
 #define ll long long
-#define N 500000
+#define N 50000000
 using namespace std;
 void fast()
 {
     ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(0);cout.tie(0);
 }
-int sz;
-vector<int>en(N);
+int sz,disword;
+vector<int>en(N);vector<int>cnt(N);
 vector<vector<int>>node(N,vector<int>(27));
 void insert(string s)
 {
     int v=0;
-    for(int i=0;i<s.size();i++)
+    for(int i=0;s[i];i++)
     {
+        cnt[v]++;
         int c=s[i]-'a';
         if(!node[v][c])
             node[v][c]=++sz;
         v=node[v][c];
     }
-    en[v]++;
+    cnt[v]++;
+    disword+=(++en[v]==1);
 }
 bool search(string s)
 {
     int v=0;
-    for(int i=0;i<s.size();i++)
+    for(int i=0;s[i];i++)
     {
         int c=s[i]-'a';
         if(!node[v][c])
             return false;
         v=node[v][c];
     }
-    return en[v]>0;
+    return en[v];
+}
+void erase(string s)
+{
+    int v=0;
+    for(int i=0;i<s[i];i++)
+    {
+        cnt[v]--;
+        int c=s[i]-'a';
+        int nx=node[v][c];
+        if(cnt[nx]==1)
+        {
+            node[v][c]=0;
+        }
+        v=nx;
+    }
+    cnt[v]--;
+    disword-=(--en[v]==0);
 }
 int main()
 {
@@ -47,6 +66,7 @@ int main()
         cin>>x;
         insert(x);
     }
+    erase(x);
     while(m--)
     {
         cin>>x;
