8d82bed794bea65367a222189b42487535e7e61b
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-10 21:15:26 -0800

    Update Power.cpp

--- Math/Power.cpp
+++ Math/Power.cpp
@@ -1,17 +1,18 @@
 template<class T>
-T power(T base, T exp, T mod)
+T power(T a, T b, T mod)
 {
     T ans = 1;
-    base %= mod;
+    a %= mod;
 
-    while (exp) 
+    while (b) 
     {
-        if (exp & 1) ans = (ans * base) % mod;
-        exp >>= 1;
-        base = (base * base) % mod;
+        if (b & 1) ans = (ans * a) % mod;
+        b >>= 1;
+        a = (a * a) % mod;
     }
 
     return ans;
 }
+//  O(log b)
 // int x=3,y=2;   cout<<power(x,y,1000);
 // ll a=2,b=3;    cout<<power(a,b,1000ll);
