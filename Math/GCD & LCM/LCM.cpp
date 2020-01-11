7cb6fa4834696942dec215c2cce16ed3481b41f4
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-10 18:43:24 -0800

    Create LCM.cpp

---
+++ Math/GCD & LCM/LCM.cpp
@@ -0,0 +1,23 @@
+// LCM (Least Common Multiple) 
+#include <bits/stdc++.h>
+#define ll long long
+#define N 50000000
+using namespace std;
+int gcd(int a,int b)
+{
+    if(b==0)
+        return a;
+    return gcd(b,a%b);
+}
+int lcm(int a,int b)
+{
+    return a*b/gcd(a,b);
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a,b;cin>>a>>b;
+    cout<<lcm(a,b);
+    return 0;
+}
