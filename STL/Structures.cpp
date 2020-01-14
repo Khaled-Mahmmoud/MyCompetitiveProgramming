297af357a4499d0ca58c24b40739828ca7263fd6
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-14 06:27:18 -0800

    Create Structures.cpp

---
+++ STL/Structures.cpp
@@ -0,0 +1,11 @@
+/*
+What is a structure?
+A structure is a user-defined data type in C/C++. A structure creates a data type that can be used to group items of possibly different 
+types into a single type
+*/
+struct myproject  // where struct is sruct keyword and myproject is structure tag
+{
+    char name[10];   // member or fields of structure
+    int id[5];       // member or fields of structure
+    float salary;    // member or fields of structure
+};
