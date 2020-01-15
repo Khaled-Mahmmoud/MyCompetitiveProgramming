d341163cb69b7c35b1e178eda11b696d367c56ab
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-14 23:26:31 -0800

    Update and rename String3 to String3.cpp

--- Containers/String3.cpp
+++ Containers/String3.cpp
@@ -1,7 +1,7 @@
-************************* Modifiers **************************
-replace
-Replaces the portion of the string that begins at character pos and spans len characters
-(or the part of the string in the range between [i1,i2] ) by new contents
+//************************* Modifiers **************************
+//replace
+//Replaces the portion of the string that begins at character pos and spans len characters
+//(or the part of the string in the range between [i1,i2] ) by new contents
 
   string base="this is a test string.";
   string str2="n example";
@@ -18,7 +18,7 @@ Replaces the portion of the string that begins at character pos and spans len ch
   str.replace(8,6,"a shorty",7);  // "this is a short phrase."    (4)
   str.replace(22,1,3,'!');        // "this is a short phrase!!!"  (5)
 
-  // Using iterators:                                               0123456789*123456789*
+  // Using iterators:                                              
   str.replace(str.begin(),str.end()-3,str3);                    // "sample phrase!!!"      (1)
   str.replace(str.begin(),str.begin()+6,"replace");             // "replace phrase!!!"     (3)
   str.replace(str.begin()+8,str.begin()+14,"is coolness",7);    // "replace is cool!!!"    (4)
@@ -26,4 +26,21 @@ Replaces the portion of the string that begins at character pos and spans len ch
   str.replace(str.begin()+11,str.end(),str4.begin(),str4.end());// "replace is useful."    (6)
   std::cout << str << '\n';
   
-Complexity : linear in the new string length
+//Complexity : linear in the new string length
+
+//erase
+//Erases part of the string, reducing its length
+
+  std::string str ("This is an example sentence.");
+  std::cout << str << '\n';
+                                           // "This is an example sentence."
+  str.erase (10,8);                        //            ^^^^^^^^
+  std::cout << str << '\n';
+                                           // "This is an sentence."
+  str.erase (str.begin()+9);               //           ^
+  std::cout << str << '\n';
+                                           // "This is a sentence."
+  str.erase (str.begin()+5, str.end()-9);  //       ^^^^^
+  std::cout << str << '\n';
+                                           // "This sentence."
+//Complexity : linear in the new string length
