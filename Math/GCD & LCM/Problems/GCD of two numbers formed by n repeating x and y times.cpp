7f17cf2e0a8e1da79391d7e4014ecd934ee482a6
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-16 10:07:06 -0800

    Update GCD of two numbers formed by n repeating x and y times.cpp

--- Math/GCD & LCM/Problems/GCD of two numbers formed by n repeating x and y times.cpp
+++ Math/GCD & LCM/Problems/GCD of two numbers formed by n repeating x and y times.cpp
@@ -2,7 +2,14 @@
 Given three positive integer n, x, y. The task is to print Greatest Common Divisor of numbers formed by n repeating x times 
 and number formed by n repeating y times.    0 <= n, x, y <= 1000000000
 
-GCD(f(n, x), f(n, y)) = f(n, GCD(x, y))
+Input : n = 123, x = 2, y = 3.
+Output : 123
+Number formed are 123123 and 123123123.
+Greatest Common Divisor of 123123 and
+123123123 is 123.
+
+
+Solution : GCD(f(n, x), f(n, y)) = f(n, GCD(x, y))
 */
 
 #include <bits/stdc++.h>
