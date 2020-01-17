e49d33921ee14fca10d7784f81ddda34dbf35b10
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-17 06:48:44 -0800

    Update and rename GCD.cpp to GCD & LCM.cpp

--- Math/GCD & LCM/GCD & LCM.cpp
+++ Math/GCD & LCM/GCD & LCM.cpp
@@ -57,3 +57,37 @@ int main()
     cout<<gcd(a,b);
     return 0;
 }
+/*
+two integers a and b are said to be relatively prime, mutually prime, or coprime 
+if gcd(a,b) = 1
+*/
+
+
+
+
+
+// LCM (Least Common Multiple) 
+#include <bits/stdc++.h>
+#define ll long long
+#define N 50000000
+using namespace std;
+int gcd(int a,int b)
+{
+    if(b==0)
+        return a;
+    return gcd(b,a%b);
+}
+int lcm(int a,int b)
+{
+    return a*b/gcd(a,b);
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a,b;cin>>a>>b;
+    cout<<lcm(a,b);
+    return 0;
+}
+//           GCD(LCM (x, y), LCM (x, z)) = LCM(x, GCD(y, z))
+//gengeral   GCD(f(x,y),f(x,z)) = f(x,GCD(y,z))   where f is fuction
