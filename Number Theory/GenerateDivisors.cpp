50e0c0db890e8381d9033a2b9badf31f13c1bc3b
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-17 02:12:10 -0800

    Update GenerateDivisors.cpp

--- Number Theory/GenerateDivisors.cpp
+++ Number Theory/GenerateDivisors.cpp
@@ -15,3 +15,22 @@ vector<T> getDivisors(T n)
 }
 
 // Complexity : O(sqrt(n))
+
+// =====================================================================================
+
+// divs[i] : holds a list of all the divisors of integer "i".
+vector<int> divs[N];
+
+/**
+ * Generates all the divisors of the integers from 1 to "n".
+ * After calling this function, "divs[i]" will contains all the divisors of integer "i".
+ *
+ * Complexity: O(n.log(n))
+ */
+void generateDivisors(int n) {
+    for (int i = 1; i <= n; ++i)
+        for (int j = i; j <= n; j += i)
+            divs[j].push_back(i);
+}
+
+// =====================================================================================
