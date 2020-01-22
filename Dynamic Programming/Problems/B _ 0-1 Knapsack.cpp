8d4aee4799b3b7a69199d4d870d9f6d0fa315700
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-22 08:37:45 -0800

    Update B _ 0-1 Knapsack.cpp

--- Dynamic Programming/Problems/B _ 0-1 Knapsack.cpp
+++ Dynamic Programming/Problems/B _ 0-1 Knapsack.cpp
@@ -7,13 +7,27 @@ In other words, given two integer arrays val[0..n-1] and wt[0..n-1] which repres
 respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that 
 sum of the weights of this subset is smaller than or equal to W
 You cannot break an item, either pick the complete item, or don’t pick it (0-1 property)
-geeksfrogeeks problem link : https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem/0
+
+GeeksfroGeeks problem link : https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem/0
 1 ≤ N ≤ 1000
 1 ≤ W ≤ 1000
 1 ≤ wt[i] ≤ 1000
 1 ≤ val[i] ≤ 1000
 */
-// Tabulation Method – Bottom Up Dynamic Programming 
+
+/*
+Tabulation Method – Bottom Up Dynamic Programming 
+------------------------------------------
+val  wt | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
+(0)  0  | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
+(1)  1  | 0 | 1 | 1 | 1 | 1 | 1 | 1 | 1 |
+(4)  3  | 0 | 1 | 1 | 4 | 5 | 5 | 5 | 5 |
+(5)  4  | 0 | 1 | 1 | 4 | 5 | 6 | 6 | 9 |
+(7)  5  | 0 | 1 | 1 | 4 | 5 | 7 | 8 | 9 |
+------------------------------------------
+
+
+*/
 int main()
 {
     int n,w;
