2a1d820fb07bb83f8ae0ae9b1f6ee930c8eb006a
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-23 19:21:52 -0800

    Update B _ Number Spiral.cpp

--- Math/Problems/B _ Number Spiral.cpp
+++ Math/Problems/B _ Number Spiral.cpp
@@ -30,9 +30,8 @@ Output:
         long long a,b,m,x;
         cin>>a>>b;
         m=max(a,b);
-        if(m%2)x=-1;
-        else x=1;
-        cout<<fixed<<setprecision(0);
+        if(m & 2) x = -1;
+        else x = 1;
         cout<<(a-b)*x+m*m-m+1<<'\n';
     }
     return 0;
