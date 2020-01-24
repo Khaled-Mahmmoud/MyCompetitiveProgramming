e042891356c764db6a4e1ec3f5076fa7e36ae689
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-24 15:09:25 -0800

    Update D _ N-Queen Problem.cpp

--- Backtracking/Problems/D _ N-Queen Problem.cpp
+++ Backtracking/Problems/D _ N-Queen Problem.cpp
@@ -3,9 +3,11 @@ The N Queen is the problem of placing N chess queens on an N×N chessboard so th
 
 Solution :
 Backtracking Algorithm
-The idea is to place queens one by one in different columns, starting from the leftmost column. When we place a queen in a column
-we check for clashes with already placed queens. In the current column, if we find a row for which there is no clash, we mark this 
-row and column as part of the solution. If we do not find such a row due to clashes then we backtrack and return false
+The idea is to place queens one by one in different columns, starting from the leftmost column
+When we place a queen in a column , we check for clashes with already placed queens
+In the current column, if we find a row for which there is no clash, we mark this 
+row and column as part of the solution. If we do not find such a row due to clashes 
+then we backtrack and return false
 
 1) Start in the leftmost column
 2) If all queens are placed
