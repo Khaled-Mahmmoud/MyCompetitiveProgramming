8b20ac284380ba8b6dd35bbab02a62451e38b046
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-23 19:20:03 -0800

    Create B _ Number Spiral.cpp

---
+++ Math/Problems/B _ Number Spiral.cpp
@@ -0,0 +1,38 @@
+/*
+A number spiral is an infinite grid whose upper-left square has number 1. Here are the first five layers of the spiral:
+
+              1    2    9    10    25
+              4    3    8    11    24
+              5    6    7    12    23
+              16   15   14   13    22
+              17   18   19   20    21
+              
+Your task is to find out the number in row y and column x
+
+1 <= t <= 10^5
+1 <= x,y <= 10^9
+
+Input:
+3
+2 3
+1 1
+4 2
+
+Output:
+8
+1
+15
+
+*/
+
+    int t;cin>>t;while(t--)
+    {
+        long long a,b,m,x;
+        cin>>a>>b;
+        m=max(a,b);
+        if(m%2)x=-1;
+        else x=1;
+        cout<<fixed<<setprecision(0);
+        cout<<(a-b)*x+m*m-m+1<<'\n';
+    }
+    return 0;
