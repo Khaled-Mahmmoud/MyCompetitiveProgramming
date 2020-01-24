dd906e24e752afaa558fa7327471b1ea811acd1f
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-24 15:10:05 -0800

    Update D _ N-Queen Problem.cpp

--- Backtracking/Problems/D _ N-Queen Problem.cpp
+++ Backtracking/Problems/D _ N-Queen Problem.cpp
@@ -1,5 +1,6 @@
 /*
-The N Queen is the problem of placing N chess queens on an N×N chessboard so that no two queens attack each other
+The N Queen is the problem of placing N chess queens on an N×N chessboard so that no two 
+queens attack each other
 
 Solution :
 Backtracking Algorithm
