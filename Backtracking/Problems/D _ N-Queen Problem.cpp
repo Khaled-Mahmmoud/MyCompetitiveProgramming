0b6a2e595ab7901dc2a424c860821f677e20f99b
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-24 12:11:19 -0800

    Update and rename D _ N-Queen Problem to D _ N-Queen Problem.cpp

--- Backtracking/Problems/D _ N-Queen Problem.cpp
+++ Backtracking/Problems/D _ N-Queen Problem.cpp
@@ -24,3 +24,64 @@ row and column as part of the solution. If we do not find such a row due to clas
 3) If all rows have been tried and nothing worked, 
    return false to trigger backtracking
 */
+
+int n;int board[10][10];
+void print(int board[10][10])
+{
+    static int k = 1;
+    cout<<k++<<'\n';
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+            cout<<board[i][j]<<' ';
+        cout<<'\n';
+    }
+    cout<<'\n';
+}
+bool save(int board[10][10],int row,int col)
+{
+    int i, j;
+    /* Check this row on left side */
+    for (i = 0; i < col; i++)
+        if (board[row][i])
+            return false;
+
+    /* Check upper diagonal on left side */
+    for (i=row, j=col; i>=0 && j>=0; i--, j--)
+        if (board[i][j])
+            return false;
+
+    /* Check lower diagonal on left side */
+    for (i=row, j=col; j>=0 && i<n; i++, j--)
+        if (board[i][j])
+            return false;
+
+    return true;
+}
+bool solve(int board[10][10],int col)
+{
+    if(col == n)
+    {
+        print(board);
+        return true;
+    }
+    bool res = false;
+    for(int i=0;i<n;i++)
+    {
+        if(save(board,i,col))
+        {
+            board[i][col]=1;
+            res = solve(board,col+1)||res;
+            board[i][col]=0;
+        }
+    }
+    return res;
+}
+int main()
+{
+    cin>>n;
+    memset(board,0,sizeof(board));
+    if(!solve(board,0))
+        cout<<"Solution does not exist";
+    return 0;
+}
