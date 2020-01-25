bd6c56c230da9eda04f41759b81d4db753c37264
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-25 06:29:03 -0800

    Update and rename A1 _ Structures.cpp to Structures.cpp

--- STL/Structures.cpp
+++ STL/Structures.cpp
@@ -61,3 +61,68 @@ int main()
    struct Point p1;  // The variable p1 is declared like a normal variable 
 }
 //Note: In C++, the struct keyword is optional before in declaration of a variable
+
+// How to access structure elements?
+// Structure members are accessed using dot (.) operator
+
+#include <iostream> 
+using namespace std; 
+  
+struct Point { 
+    int x, y; 
+}; 
+  
+int main() 
+{ 
+    struct Point p1 = { 0, 1 }; 
+  
+    // Accessing members of point p1 
+    p1.x = 20; 
+    cout << p1.x << " " << p1.y; // 20  1
+  
+    return 0; 
+}
+// What is an array of structures?
+// Like other primitive data types, we can create an array of structures.
+
+#include <iostream> 
+using namespace std; 
+  
+struct Point { 
+    int x, y; 
+}; 
+  
+int main() 
+{ 
+    // Create an array of structures 
+    struct Point arr[10]; 
+  
+    // Access array members 
+    arr[0].x = 10; 
+    arr[0].y = 20; 
+  
+    cout << arr[0].x << " " << arr[0].y; 
+    return 0; 
+} 
+/*
+What is a structure pointer?
+Like primitive types, we can have pointer to a structure. If we have a pointer to structure, members are accessed using
+arrow ( -> ) operator instead of the dot (.) operator
+
+*/
+struct Point { 
+    int x, y; 
+}; 
+  
+int main() 
+{ 
+    struct Point p1 = { 1, 2 }; 
+  
+    // p2 is a pointer to structure p1 
+    struct Point* p2 = &p1; 
+  
+    // Accessing structure members using structure pointer 
+    
+    cout << p2->x << " " << p2->y;       //    1   2
+    return 0; 
+}
