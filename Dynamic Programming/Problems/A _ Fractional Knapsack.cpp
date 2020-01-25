7a9fa60a6a2286b66d845bd21ec4abcead973e36
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-25 04:45:37 -0800

    Update A _ Fractional Knapsack.cpp

--- Dynamic Programming/Problems/A _ Fractional Knapsack.cpp
+++ Dynamic Programming/Problems/A _ Fractional Knapsack.cpp
@@ -1,7 +1,7 @@
 /*
 ***************** Fractional Knapsack Problem *****************
-Given weights and values of n items, we need to put these items in a knapsack of capacity W to get the maximum total value in
-the knapsack
+Given weights and values of n items, we need to put these items in a knapsack
+of capacity W to get the maximum total value in the knapsack
 
   Input:
   Items as (value, weight) pairs
@@ -11,8 +11,8 @@ the knapsack
   Maximum possible value = 220
   by taking items of weight 20 and 30 kg
   
-In Fractional Knapsack, we can break items for maximizing the total value of knapsack. This problem in which we can break an item is
-also called the fractional knapsack problem
+In Fractional Knapsack, we can break items for maximizing the total value of knapsack
+This problem in which we can break an item is also called the fractional knapsack problem
 
    Input : 
    Same as above
@@ -21,9 +21,11 @@ also called the fractional knapsack problem
    By taking full items of 10 kg, 20 kg and 
    2/3rd of last item of 30 kg
    
-An efficient solution is to use Greedy approach. The basic idea of the greedy approach is to calculate the ratio value/weight for 
-each item and sort the item on basis of this ratio. Then take the item with the highest ratio and add them until we can’t add the next 
-item as a whole and at the end add the next item as much as we can. Which will always be the optimal solution to this problem
+An efficient solution is to use Greedy approach. The basic idea of the greedy approach is 
+to calculate the ratio value/weight for each item and sort the item on basis of this ratio
+Then take the item with the highest ratio and add them until we can’t add the next item as
+a whole and at the end add the next item as much as we can. Which will always be the optimal
+solution to this problem
 
 */
 
