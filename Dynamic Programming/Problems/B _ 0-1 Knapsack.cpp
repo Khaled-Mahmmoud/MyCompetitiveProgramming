0ed423e04516ae8f7887a7ff4fd57866771a5143
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-25 05:19:47 -0800

    Update B _ 0-1 Knapsack.cpp

--- Dynamic Programming/Problems/B _ 0-1 Knapsack.cpp
+++ Dynamic Programming/Problems/B _ 0-1 Knapsack.cpp
@@ -1,12 +1,9 @@
 /*
-#########
-22/1/2020
-#########
-Given weights and values of n items , put these items in a knapsack of capacity W to get the maximum total value in the knapsack
-In other words, given two integer arrays val[0..n-1] and wt[0..n-1] which represent values and weights associated with n items 
-respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that 
-sum of the weights of this subset is smaller than or equal to W
-You cannot break an item, either pick the complete item, or don’t pick it (0-1 property)
+Given weights and values of n items , put these items in a knapsack of capacity W to get the maximum total 
+value in the knapsack In other words, given two integer arrays val[0..n-1] and wt[0..n-1] which represent
+values and weights associated with n items respectively. Also given an integer W which represents knapsack
+capacityfind out the maximum value subset of val[] such that sum of the weights of this subset is smaller
+than or equal to W You cannot break an item, either pick the complete item, or don’t pick it (0-1 property)
 
 GeeksfroGeeks problem link : https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem/0
 1 ≤ N ≤ 1000
