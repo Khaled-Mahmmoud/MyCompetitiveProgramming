41f7439d6332df3d44bd7f6f9a85cf5d46e985ad
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-01-31 17:22:49 -0800

    Update and rename Fermat’s Last Theorem to Theorems.cpp

---
+++ Number Theory/Theorems.cpp
@@ -0,0 +1,61 @@
+/*
+Fermat’s Last Theorem
+According to Fermat’s Last Theorem, no three positive integers a, b, c satisfy the equation
+a^n + b^n = c^n for any integer value of n greater than 2
+For n = 1 and n = 2, the equation have infinitely many solutions
+
+Some solutions for n = 1 are,
+ 2 + 3 = 5
+ 7 + 13 = 20
+ 5 + 6 = 11
+ 10 + 9 = 19
+
+Some solutions for n = 2 are,
+5^2 + 12^2 = 13^2     
+3^2 + 4^2 = 5^2    
+8^2 + 15^2 = 17^2    
+9^2 + 40^2 = 41^2 
+
+
+Lemoine’s Conjecture Theorem
+Any odd integer greater than 5 can be expressed as a sum of an odd prime (all primesother than 2 are odd)
+and an even semiprime. A semiprime number is a product of two prime numbers
+
+7 = 3 + (2 × 2),
+where 3 is a prime number and 4 (= 2 × 2) is a semiprime number
+11 = 5 + (2 × 3)
+where 5 is a prime number and 6(= 2 × 3) is a semiprime number
+9 = 3 + (2 × 3) or 9 = 5 + (2 × 2)
+47 = 13 + 2 × 17 = 37 + 2 × 5 = 41 + 2 × 3 = 43 + 2 × 2
+
+*/
+void lemoine(int n) 
+{ 
+    map<int, int> pr; 
+    map<int, int>::iterator it; 
+    it = pr.begin(); 
+      
+    // Finding various values of p for each q 
+    // to satisfy n = p + (2 * q) 
+    for (int q = 1; q <= n / 2; q++)  
+    {  
+        int p = n - 2 * q; 
+        if (isPrime(p) && isPrime(q)) 
+            pr.insert(it, pair<int, int>(p, q)); 
+    } 
+    for (it = pr.begin(); it != pr.end(); ++it) 
+        cout << n << " = " << it->first << " + (2 * " << it->second << ")\n"; 
+} 
+int main() 
+{ 
+    int n = 39; 
+    lemoine(n);
+    return 0; 
+}
+/*
+output :
+39 = 5 + (2 * 17)
+39 = 13 + (2 * 13)
+39 = 17 + (2 * 11)
+39 = 29 + (2 * 5)
+*/
