f354a4a6fb39e78fdcaecb9914285129aeacb88f
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-02-01 08:36:49 -0800

    Update A _ Find politeness of a number.cpp

--- Number Theory/Prime Factorization and Divisors Problems/A _ Find politeness of a number.cpp
+++ Number Theory/Prime Factorization and Divisors Problems/A _ Find politeness of a number.cpp
@@ -19,26 +19,25 @@ There are two ways of representation:
 9 = 2 + 3 + 4
 9 = 4 + 5
 Naive approach is to run a loop one inside another and find the sum of every consecutive integers up to n
-Time complexity of this approach will be O(n^2) which will not be sufficient for large value of n.
+Time complexity of this approach will be O(n^2) which will not be sufficient for large value of n
 
 Efficient approach is to use factorization. We factorize the number n and count the number of odd factors
-Total number of odd factors (except 1) is equal to politeness of the number. Refer this for proof of this fact
-In general if a number can be represented as ap * bq * cr … where a, b, c, … are prime factors of n
+Total number of odd factors (except 1) is equal to politeness of the number. 
+In general if a number can be represented as a^p * b^q * c^r … where a, b, c, … are prime factors of n
 If a = 2 (even) then discard it and count total number of odd factors which can be written 
 as [(q + 1) * (r + 1) * …] – 1 (Here 1 is subtracted because single term in representation is not allowed)
 
-
-
-How does above formula work? The fact is, if a number is expressed as ap * bq * cr … where a, b, c, … are prime factors of n
-then number of divisors is (p+1)*(q+1)*(r+1) ……To simplify, let there be one factor and number is expressed as ap
-Divisors are 1, a, a2, …. ap. The count of divisors is p+1
-Now let us take a slightly more complicated case apbp. The divisors are :
-1, a, a2, …. ap
-b, ba, ba2, …. bap
-b2, b2a, b2a2, …. b2ap
+How does above formula work? The fact is, if a number is expressed as a^p * b^q * c^r … where a, b, c, … are prime factors of n
+then number of divisors is (p+1)*(q+1)*(r+1) ……To simplify, let there be one factor and number is expressed as a^p
+Divisors are 1, a, a^2, …. a^p . The count of divisors is p+1
+Now let us take a slightly more complicated case a^p b^p
+The divisors are :
+1, a, a^2, …. a^p
+b, ba, ba^2, …. ba^p
+b^2, b^2a, b^2a^2, …. b^2a^p
 …………….
 …………….
-bq, bqa, bqa2, …. bqap
+b^q, b^qa, b^qa^2, …. b^qa^p
 
 The count of above terms is (p+1)*(q+1). Similarly, we can prove for more prime factors.
 
