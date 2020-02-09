db219936002ea2526b829b5bd2b40ecdd155b8a9
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-02-09 06:11:04 -0800

    Create Fibonacci numbers.cpp

---
+++ Math/Fibonacci numbers.cpp
@@ -0,0 +1,37 @@
+/*
+
+The Fibonacci numbers are the numbers in the following integer sequence.
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+
+In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation
+
+
+
+    Fn = Fn-1 + Fn-2
+with seed values   F0 = 0 and F1 = 1
+
+
+*/
+
+int fib(int n) 
+{ 
+  double phi = (1 + sqrt(5)) / 2; 
+  return round(pow(phi, n) / sqrt(5)); 
+}
+// Complexity : O(1)
+// Notice , we can use pow but we can't use Power bcause phi is double
+// but if answer modulo mod , we use this code
+vector<ll> f(10000);
+ll fib(ll n)
+{
+    if(n<2)
+    return n;
+    if(n==2)
+        return 1;
+    if(f[n])
+        return f[n];
+    ll k = (n&1) ? (n+1)/2 : n/2 ;
+    return f[n] =( (n&1) ? (fib(k)*fib(k) + fib(k-1)*fib(k-1)) : (2*fib(k-1)+fib(k))*fib(k) )%mod;
+
+}
+// Complexity : O(log(n))
