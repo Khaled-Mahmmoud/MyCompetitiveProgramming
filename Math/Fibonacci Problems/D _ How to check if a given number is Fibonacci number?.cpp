9d435ef95cb771fbacff20b3c2cd36b55ac1462d
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-02-09 08:35:02 -0800

    Update D _ How to check if a given number is Fibonacci number?.cpp

--- Math/Fibonacci Problems/D _ How to check if a given number is Fibonacci number?.cpp
+++ Math/Fibonacci Problems/D _ How to check if a given number is Fibonacci number?.cpp
@@ -17,7 +17,7 @@ Output : No
 Recommended: Please solve it on “PRACTICE ” first, before moving on to the solution.
 A simple way is to generate Fibonacci numbers until the generated number is greater than or equal to ‘n’
 Following is an interesting property about Fibonacci numbers that can also be used to check if a given number is Fibonacci or not
-A number is Fibonacci if and only if one or both of (5*n2 + 4) or (5*n2 – 4) is a perfect square (Source: Wiki)
+A number is Fibonacci if and only if one or both of (5*n^2 + 4) or (5*n^2 – 4) is a perfect square (Source: Wiki)
 Following is a simple program based on this concept
 
 
