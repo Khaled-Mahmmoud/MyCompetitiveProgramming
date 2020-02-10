2e6d6cd844b1cbdf46da5b84e3c2b888867c3bc8
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-02-10 14:47:30 -0800

    Update A _ Modular multiplicative inverse.cpp

--- Number Theory/Modular Arithmetic Problems/A _ Modular multiplicative inverse.cpp
+++ Number Theory/Modular Arithmetic Problems/A _ Modular multiplicative inverse.cpp
@@ -65,11 +65,11 @@ int gcd(int a, int b, int *x, int *y)
     }  
   
     int x1, y1;  
-    int gcd = gcd(b%a, a, &x1, &y1);   
+    int g = gcd(b%a, a, &x1, &y1);   
     *x = y1 - (b/a) * x1;  
     *y = x1;  
   
-    return gcd; 
+    return g; 
 }
 int main() 
 { 
