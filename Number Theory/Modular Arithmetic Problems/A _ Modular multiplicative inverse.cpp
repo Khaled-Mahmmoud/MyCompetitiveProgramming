7b1f72e5f198ee0e6140ca8ca41a37fa20795fb4
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-02-11 09:19:08 -0800

    Update A _ Modular multiplicative inverse.cpp

--- Number Theory/Modular Arithmetic Problems/A _ Modular multiplicative inverse.cpp
+++ Number Theory/Modular Arithmetic Problems/A _ Modular multiplicative inverse.cpp
@@ -4,7 +4,7 @@ Modular multiplicative inverse
 Given two integers ‘a’ and ‘m’, find modular multiplicative inverse of ‘a’ under modulo ‘m’.
 
 The modular multiplicative inverse is an integer ‘x’ such that.
- a x ≡ 1 (mod m) 
+ a * x ≡ 1 (mod m) 
 The value of x should be in {0, 1, 2, … m-1}, i.e., in the range of integer modulo m.
 The multiplicative inverse of “a modulo m” exists if and only if a and m are relatively prime (i.e., if gcd(a, m) = 1).
 
