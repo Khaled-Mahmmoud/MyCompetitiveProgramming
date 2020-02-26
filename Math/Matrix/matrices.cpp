379639d78285397bc85f1b3502586bc0251bd68a
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-02-26 02:42:42 -0800

    Update matrices.cpp

--- Math/Matrix/matrices.cpp
+++ Math/Matrix/matrices.cpp
@@ -1,5 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
 #define ll long long
 #define sz(v) (int)(v.size())
 
@@ -10,10 +8,31 @@ matrix initial(int n, int m, int val = 0)
 {
 	return matrix(n, row(m, val));
 }
+/*
+Zero Matrix 
+matrix with all values are zero
+|0 0 0|
+|0 0 0|
+|0 0 0|
+*/
 matrix zero(int n,int m)
 {
 	return initial(n,m);
 }
+/*
+identity matrix
+The n X n, identity matrix, denoted I_n 
+The entries on the diagonal from the upper left to the bottom right are all 1's, and all other entries are 0
+
+ I2 = |1  0|               |1  0  0|
+      |0  1|          I3 = |0  1  0|                    |1  0  0  0|
+                           |0  0  1|               I4 = |0  1  0  0|
+                                                        |0  0  1  0|
+                                                        |0  0  0  1|
+							
+The product of any square matrix and the appropriate identity matrix is always the original matrix
+regardless of the order in which the multiplication was performed! In other words, A.I = I.A = A
+*/
 matrix identity(int n) 
 {
 	matrix rt = initial(n, n);
@@ -38,7 +57,19 @@ matrix add(const matrix& a, const matrix& b)
 		rt[i][j] = a[i][j] + b[i][j];
 	return rt;
 }
+/*
+           Matrix multiplication dimensions
+The dimensions of a matrix give the number of rows and columns of the matrix in that order
+Since matrix A has 2 rows and 3 columns, it is called 2 * 3 matrix
 
+When is matrix multiplication defined?
+In order for matrix multiplication to be defined, the number of columns in the first matrix must 
+be equal to the number of rows in the second matrix
+
+          (m X n).(n X k)
+    product matrix is dimension of m X k
+    
+*/
 matrix multiply(const matrix& a, const matrix& b) 
 {
 	matrix rt = initial(sz(a), sz(b[0]));
@@ -47,6 +78,20 @@ matrix multiply(const matrix& a, const matrix& b)
              for(k=0;k<sz(a[0]);k++)
                 rt[i][j]+=a[i][k]*b[k][j];
 	return rt;
+}
+/*
+Matrix Trace
+sum of diagonal values
+10  1   2
+-5  20  7
+0   0   30
+Trace = 10 + 20 +30 = 60
+*/
+int matrixTrace(int a,int b)
+{
+	int ret = 0;
+	for(int i=0;i<sz(a);i++)ret += a[i][i];
+	return ret;
 }
 matrix power(const matrix& a, ll k)
 {
