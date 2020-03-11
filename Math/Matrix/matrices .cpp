1890a902986a6e210543d66cb1281a008c313f26
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-03-11 03:22:35 -0700

    Update matrices .cpp

--- Math/Matrix/matrices .cpp
+++ Math/Matrix/matrices .cpp
@@ -87,6 +87,7 @@ matrix multiply(const matrix& a, const matrix& b)
                 rt[i][j]+=a[i][k]*b[k][j];
 	return rt;
 }
+
 /*
 Matrix Trace
 sum of diagonal values
@@ -100,4 +101,32 @@ int matrixTrace(const matrix& a)
 	int ret = 0;
 	for(int i=0;i<sz(a);i++)ret += a[i][i];
 	return ret;
+}
+/*
+Matrix ClockWise Rotation
+
+ 1  7  4           0  1
+ 0  9  4    -->    9  7
+                   4  4
+*/  
+matrix rotate(matrix v)
+{
+	  matrix r = zero(sz(v[0]),sz(v));
+    for(int i=0;i<sz(v);i++)
+        for(int j=0;i<sz(v[0]);j++)
+            r[j][sz(v)-i-1] = v[i][j];
+    return r;
+}
+/*
+Matrix Reflection
+ 1  7  4           4  7  1
+ 0  9  4    -->    4  9  0
+ */
+matrix Reflect(matrix v)
+{
+	  matrix r = zero(sz(v),sz(v[0]));
+    for(int i=0;i<sz(v);i++)
+        for(int j=0;i<sz(v[0]);j++)
+            r[i][sz(v[0])-j-1] = v[i][j];
+    return r;
 }
