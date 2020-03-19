6e05b3b965ac15796b51c09d9b827e65b393de8c
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-03-19 08:09:27 -0700

    Create E _ Maximum LCM of three <= n.cpp

---
+++ Math/GCD & LCM Problems/E _ Maximum LCM of three <= n.cpp
@@ -0,0 +1,20 @@
+/*
+the maximum possible LCM of three not necessarily distinct positive integers that are not greater than n
+codeforces problem link : https://codeforces.com/contest/235/problem/A
+*/
+#include <bits/stdc++.h>
+#define ll long long
+#define N  1000000
+using namespace std;
+vector<int>d(N);
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    ll n;cin>>n;
+    if(n<3)cout<<n;
+    else if(n%2)cout<<n*(n-1)*(n-2);
+    else if(n%3==0)cout<<(n-1)*(n-2)*(n-3);
+    else cout<<n*(n-1)*(n-3);
+    return 0;
+}
