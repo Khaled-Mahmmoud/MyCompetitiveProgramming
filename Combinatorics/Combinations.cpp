ed65162bcc21a65d13aa0df61f4e31cc518991f2
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-03-21 10:11:36 -0700

    Update Combinations.cpp

--- Combinatorics/Combinations.cpp
+++ Combinatorics/Combinations.cpp
@@ -37,7 +37,6 @@ The Problem
 Write a function that takes two parameters n and k and returns the value of Binomial Coefficient C(n, k)
 For example, your function should return 6 for n = 4 and k = 2, and it should return 10 for n = 5 and k = 2.
 
-Recommended: Please solve it on “PRACTICE” first, before moving on to the solution.
 1) Optimal Substructure
 The value of C(n, k) can be recursively calculated using following standard formula for Binomial Coefficients.
 
