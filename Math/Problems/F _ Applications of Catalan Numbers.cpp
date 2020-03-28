ca9935ee635696c427d8ea4166d38cc89322adc7
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-03-28 02:47:28 -0700

    Create F _ Applications of Catalan Numbers.cpp

---
+++ Math/Problems/F _ Applications of Catalan Numbers.cpp
@@ -0,0 +1,6 @@
+The number of paths on a rectangular grid from bottom left to top right
+i.e., (n-1, 0) to (0, n-1) that lies above the main diagonal
+
+Number of expressions containing n pairs of parentheses which are correctly matched
+For n = 3, possible expressions are ((())), ()(()), ()()(), (())(), (()())
+
