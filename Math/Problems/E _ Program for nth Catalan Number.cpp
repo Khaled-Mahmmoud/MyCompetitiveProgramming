425a04de13bee76d6633c2ab8f26e8461b0949ea
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-03-28 09:27:30 -0700

    Update E _ Program for nth Catalan Number.cpp

--- Math/Problems/E _ Program for nth Catalan Number.cpp
+++ Math/Problems/E _ Program for nth Catalan Number.cpp
@@ -1,18 +1,14 @@
 /*
 
 Program for nth Catalan Number
-Catalan numbers are a sequence of natural numbers that occurs in many interesting counting problems like following.
-1) Count the number of expressions containing n pairs of parentheses which are correctly matched
-For n = 3, possible expressions are ((())), ()(()), ()()(), (())(), (()())
-
-2) Count the number of possible Binary Search Trees with n keys (See this)
-
-3) Count the number of full binary trees 
-(A rooted binary tree is full if every vertex has either two children or no children)with n+1 leaves.
-
+Catalan numbers are a sequence of natural numbers that occurs in many interesting counting problems like following
 The first few Catalan numbers for n = 0, 1, 2, 3, … are 1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, …
+ Catalan Number = (2n)! / ((n + 1)! * n!)
+ Catalan numbers satisfy the following recursive formula
+C0 = 1 , C(n+1) = sum(i=0,n) Ci Cn-i , n >= 0
 
 */
+
 int catalan[10000];
 int catalan(int n) 
 { 
