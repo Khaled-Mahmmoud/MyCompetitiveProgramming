aa17d34ba6bd9648147eaadac5e4f79a12fef867
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-03-30 00:25:14 -0700

    Update F _ Applications of Catalan Numbers.cpp

--- Math/Problems/F _ Applications of Catalan Numbers.cpp
+++ Math/Problems/F _ Applications of Catalan Numbers.cpp
@@ -35,3 +35,15 @@ Input: n = 2
 Output: 2
 It can be cut into 2 triangles by using either pair of opposite vertices
 
+5) Given a number N . Number of Permutations of 1 to N such that no Three Terms forms Increasing Subsequence
+Input : N = 3
+Output : 5
+Valid permutations : 132, 213, 231, 312 and 321 and not 123
+
+Input : N = 4
+Output : 14
+Valid permutations : 1432, 2143, 2413, 2431, 3142, 3214, 3241, 3412, 3421, 4132, 4213, 4231, 4312 and 4321
+
+Solution : find the n’th Catalan Number. First few Catalan numbers are 1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862
+
+
