dffc2eeb9ea2b216d2268b356e8061b7b6449966
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-04-06 07:53:34 -0700

    Update Catalan Number.cpp

--- Math/Catalan Number.cpp
+++ Math/Catalan Number.cpp
@@ -49,6 +49,7 @@ int main()
 	return 0;
 }
 
+/*
      Applications of Catalan Numbers
 1) Number of paths on a rectangular grid from bottom left to top right
 i.e., (n-1, 0) to (0, n-1) that lies above the main diagonal
