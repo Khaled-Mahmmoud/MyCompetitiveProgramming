3767a46f8021ff5732428c1f4f4f9abec85f2dd5
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-05-04 18:39:33 -0700

    Create A _ Factorial.cpp

---
+++ Math/Factorial/A _ Factorial.cpp
@@ -0,0 +1,28 @@
+  
+/*
+Program for factorial of a number
+Factorial of a non-negative integer, is multiplication of all integers smaller than or equal to n
+For example factorial of 6 is 6*5*4*3*2*1 which is 720
+!n = n * (n-1) * (n-2) * (n-3) * .... * 1
+!4 = 4 * 3 * 2 * 1
+!6 = 6 * 5 * 4 * 3 * 2 * 1
+Recursive Solution:
+Factorial can be calculated using following recursive formula
+  n! = n * (n-1)!
+  0! = 1 
+  1! = 1
+  
+  
+unsigned int factorial(unsigned int n) 
+{ 
+    if(n == 0) 
+        return 1; 
+    return n * factorial(n - 1); 
+} 
+unsigned int factorial(unsigned int n) 
+{ 
+    int res = 1, i; 
+    for(i = 2; i <= n; i++) 
+        res *= i; 
+    return res; 
+} 
