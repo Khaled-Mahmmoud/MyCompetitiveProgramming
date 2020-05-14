db6b16696af5ae87c5d9c2c2a6e3d2f6686d3115
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-05-14 12:00:26 -0700

    Create E _ Pronic number.cpp

---
+++ Math/Problems/E _ Pronic number.cpp
@@ -0,0 +1,33 @@
+/*
+
+Check if a given number is Pronic
+A pronic number is a number which is the product of two consecutive integers, that is, a number of the form n(n + 1)
+The first few Pronic numbers are:
+0, 2, 6, 12, 20, 30, 42, 56, 72, 90, 110, 132, 156, 182, 210, 240, 272, 306, 342, 380, 420, 462 . . . . . .
+    
+                    *****
+             ****   *****
+      ***    ****   *****
+**    ***    ****   *****
+1*2   2*3     3*4    4*5
+The numbers that can be arranged to form a rectangle are called Rectangular Numbers (also known as Pronic numbers)
+
+*/
+
+bool checkPronic(int x) 
+{ 
+  
+    for (int i = 0;i <= sqrt(x);i++) 
+        if (x == i * (i + 1))  
+        return true; 
+  
+    return false; 
+} 
+int main(void) 
+{ 
+    for (int i = 0; i <= 100; i++)  
+        if (checkPronic(i)) 
+            cout << i << " "; 
+      
+    return 0; 
+} 
