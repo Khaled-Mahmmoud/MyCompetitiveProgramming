61c3676a76240e6692c33d3cc7f730b4fe915f88
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-06-26 18:10:27 -0700

    Update A _ Sum of two large numbers.cpp

--- Math/Large Numbers Problems/A _ Sum of two large numbers.cpp
+++ Math/Large Numbers Problems/A _ Sum of two large numbers.cpp
@@ -17,11 +17,14 @@ append the sum % 10 to end of result and keep track of carry as sum/10
 
 string findSum(string str1, string str2) 
 { 
+    int n1 = str1.length();
+    int n2 = str2.length();
+         
     if (str1.length() > str2.length()) 
         swap(str1, str2);
+         
     string str = ""; 
-    int n1 = str1.length(), n2 = str2.length(); 
-  
+         
     reverse(str1.begin(), str1.end()); 
     reverse(str2.begin(), str2.end()); 
   
