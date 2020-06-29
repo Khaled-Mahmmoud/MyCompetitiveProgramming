12223259c57d7b53b9ec0c189ddfaa14f215ae0d
Khaled Mahmoud <46223389+Khaled-M-Fawzi@users.noreply.github.com>
2020-06-29 04:34:44 -0700

    Update C _ Multiplicative order.cpp

--- Number Theory/Modular Arithmetic Problems/C _ Multiplicative order.cpp
+++ Number Theory/Modular Arithmetic Problems/C _ Multiplicative order.cpp
@@ -1,13 +1,15 @@
 /*
 
 Multiplicative order
-In number theory, given an integer A and a positive integer N with gcd( A , N) = 1
-the multiplicative order of a modulo N is the smallest positive integer k with A^k( mod N ) = 1 ( 0 < K < N )
+In number theory, given an integer a and positive integer m with gcd( a , m) = 1
+the multiplicative order of a modulo m is the smallest positive integer k with  ( 0 < K < N )
 
-Input : A = 4 , N = 7 
+    a^x = 1  (mod m)
+
+Input : a = 4 , m = 7 
 Output : 3
 explanation :  GCD(4, 7) = 1  
-               A^k( mod N ) = 1 ( smallest positive integer K )
+               a^x(mod m) = 1 ( smallest positive integer x )
                4^1 = 4(mod 7)  = 4
                4^2 = 16(mod 7) = 2
                4^3 = 64(mod 7)  = 1
@@ -15,21 +17,21 @@ explanation :  GCD(4, 7) = 1
                4^5 = 1024(mod 7)  = 2
                4^6 = 4096(mod 7)  = 1
 
-smallest positive integer K = 3  
+smallest positive integer x = 3  
 
-Input :  A = 3 , N = 1000 
+Input :  a = 3 , m = 1000 
 Output : 100  (3^100 (mod 1000) == 1) 
 
-Input : A = 4 , N = 11 
+Input : a = 4 , m = 11 
 Output : 5 
 
-IF we take a close look then we observe that we do not need to calculate power every time
-we can be obtaining next power by multiplying ‘A’ with the previous result of a module  
+If we take a close look then we observe that we do not need to calculate power every time
+we can be obtaining next power by multiplying ‘a’ with the previous result of a mod m
 
 Explanation : 
-A = 4 , N = 11  
+a = 4 , m = 11  
 initially result = 1 
-with normal                with modular arithmetic (A * result)
+with normal                with modular arithmetic (a * result)
 4^1 = 4 (mod 11 ) = 4  ||  4 * 1 = 4 (mod 11 ) = 4 [ result = 4]
 4^2 = 16(mod 11 ) = 5  ||  4 * 4 = 16(mod 11 ) = 5 [ result = 5]
 4^3 = 64(mod 11 ) = 9  ||  4 * 5 = 20(mod 11 ) = 9 [ result = 9]
@@ -37,7 +39,7 @@ with normal                with modular arithmetic (A * result)
 4^5 = 1024(mod 5 ) = 1 ||  4 * 3 = 12(mod 11 ) = 1 [ result = 1]
 
 smallest positive integer  5 
-Run a loop from 1 to N-1 and Return the smallest +ve power of A under modulo n which is equal to 1
+Run a loop from 1 to m-1 and Return the smallest +ve power of a under modulo n which is equal to 1
 
 */
 
@@ -47,27 +49,27 @@ int GCD ( int a , int b )
         return a; 
     return GCD( b , a%b ) ; 
 } 
-int multiplicativeOrder(int A, int  N) 
+int multiplicativeOrder(int a, int  m) 
 { 
-    if (GCD(A, N ) != 1) 
+    if (GCD(a, m) != 1) 
         return -1; 
-    unsigned int result = 1; 
-    int K = 1 ; 
-    while (K < N) 
+    int result = 1; 
+    int x = 1 ; 
+    while (x < m) 
     { 
-        result = (result * A) % N ; 
+        result = (result * a) % m ; 
         if (result  == 1) 
-            return K; 
-        K++; 
+            return x; 
+        x++; 
     } 
   
     return -1 ; 
 } 
 int main() 
 { 
-    int A = 4 , N = 7; 
-    cout << multiplicativeOrder(A, N); 
+    int a = 4 , m = 7; 
+    cout << multiplicativeOrder(a, m); 
     return 0; 
 } 
 
-// Time Complexity: O(N)
+// Time Complexity: O(m)
