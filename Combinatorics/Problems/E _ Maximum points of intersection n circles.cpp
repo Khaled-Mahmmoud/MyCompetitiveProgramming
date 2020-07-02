e77458d2c2e66a5103354d7699c642ef14ccb00a
Khaled Mahmoud <46223389+Khaled-Mahmmoud@users.noreply.github.com>
2020-07-02 01:56:15 -0700

    Update E _ Maximum points of intersection n circles.cpp

--- Combinatorics/Problems/E _ Maximum points of intersection n circles.cpp
+++ Combinatorics/Problems/E _ Maximum points of intersection n circles.cpp
@@ -12,7 +12,7 @@ Input :  n = 3
 Output : 6
 
 Description and Derivation
-As we can see in above diagram, for each pair of circles, there can be maximum two intersectuib points
+As we can see in above diagram, for each pair of circles, there can be maximum two intersection points
 Therefore if we have n circles then there can be nC2 pairs of circles in which each pair will have two intersections
 So by this we can conclude that by looking at all possible pairs of circles the mathematical formula can be made for
 the maximum number of intersection by n circles is given by 2 * nC2.
