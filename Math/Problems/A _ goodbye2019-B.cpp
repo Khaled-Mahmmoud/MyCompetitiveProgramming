60d8a06c39a2ee003b5e7f631ee0793c672deb7a
Khaled Mahmoud <46223389+Khaled-Mahmmoud@users.noreply.github.com>
2020-07-05 10:47:45 -0700

    Update A _ goodbye2019-B.cpp

--- Math/Problems/A _ goodbye2019-B.cpp
+++ Math/Problems/A _ goodbye2019-B.cpp
@@ -1,15 +1,15 @@
 /*
-https://codeforces.com/contest/1270/problem/B
-Tutorial
+For an array a of integers let's denote its maximal element as max(a), and minimal as min(a). 
+We will call an array a of k integers interesting if max(a)−min(a)≥k. 
+For example, array [1,3,4,3] isn't interesting as max(a)−min(a)=4−1=3<4 while array [7,3,0,4,3] is as max(a)−min(a)=7−0=7≥5.
+
+Solution:
+You are given an array a of n integers. Find some interesting nonempty subarray of a, or tell that it doesn't exist.Tutorial
 We will show that if some interesting nonempty subarray exists, then also exists some interesting subarray of length 2
 Therefore, the solution is as follows: 
 for each i from 1 to n−1 check if |a(i+1) − a(i)| ≥ 2 holds. If this is true for some i,
 we have found an interesting subarray of length 2 else such subarray doesn't exist
 */
-#include <bits/stdc++.h>
-#define ll long long
-#define N 27
-using namespace std;
 int main()
 {
     ios::sync_with_stdio(false);
