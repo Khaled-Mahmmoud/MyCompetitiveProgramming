fce6a4e1059fb38bce20cd081ba48bdce95350b9
Khaled Mahmoud <46223389+Khaled-Mahmmoud@users.noreply.github.com>
2020-07-11 16:53:08 -0700

    Update and rename U _ Number of elements with odd factors in given range.cpp to S _ Number of elements with odd factors in given range.cpp

--- Number Theory/Prime Factorization and Divisors Problems/S _ Number of elements with odd factors in given range.cpp
+++ Number Theory/Prime Factorization and Divisors Problems/S _ Number of elements with odd factors in given range.cpp
@@ -2,19 +2,10 @@
 Number of elements with odd factors in given range
 Given a range [n,m], find the number of elements that have odd number of factors in the given range (n and m inclusive).
 
-Examples :
-
 Input  : n = 5, m = 100
 Output : 8
 The numbers with odd factors are 9, 16, 25, 
 36, 49, 64, 81 and 100
-
-Input  : n = 8, m = 65
-Output : 6
-
-Input  : n = 10, m = 23500
-Output : 150
-
 
 */
 
