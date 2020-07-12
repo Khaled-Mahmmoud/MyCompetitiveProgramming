3f675d84e04e1a655afd8f4ebfa3c203eff7a28a
Khaled Mahmoud <46223389+Khaled-Mahmmoud@users.noreply.github.com>
2020-07-11 17:40:59 -0700

    Update B _ Legendre’s formula.cpp

--- Number Theory/Factorial/B _ Legendre’s formula.cpp
+++ Number Theory/Factorial/B _ Legendre’s formula.cpp
@@ -10,7 +10,7 @@ Naive approach
 The naive approach is to find the power of p in each number from 1 to n and add them
 Because we know that during multiplication power are added
 Efficient Approach
-Before discussing efficient approach lets discuss a question given a two numbers n, m how many numbers are there from 1 to n
+Before discussing efficient approach lets discuss a question given two numbers n, m how many numbers are there from 1 to n
 that are divisible by m the answer is floor(n/m). Now coming back to our original question to find the power of p in n!
 what we do is count the number of numbers from 1 to n that are divisible by p then by p^2 then by p^3 till p^k > n and add them
 This will be our required answer
