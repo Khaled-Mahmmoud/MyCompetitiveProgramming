0d23c122c42c868275651f993e3817befdd6e288
Khaled Mahmoud <46223389+Khaled-Mahmmoud@users.noreply.github.com>
2020-11-15 09:30:15 -0800

    fib

--- Math/Fibonacci Problems/L _ Sum of Fibonacci Numbers.cpp
+++ Math/Fibonacci Problems/L _ Sum of Fibonacci Numbers.cpp
@@ -1,71 +1,10 @@
-/*
-
-Sum of Fibonacci Numbers
+# Sum of Fibonacci Numbers
 Given a number positive number n, find value of f0 + f1 + f2 + …. + fn where fi indicates i’th Fibonacci number.
-Remember that f0 = 0, f1 = 1, f2 = 1, f3 = 2, f4 = 3, f5 = 5, …
 
-Input  : n = 3
-Output : 4
-Explanation : 0 + 1 + 1 + 2  = 4
 
-Input  :  n = 4
-Output :  7
-Explanation : 0 + 1 + 1 + 2 + 3  = 7
- 
+**Method** 1 (O(n)) : Brute Force approach is pretty straight forward, find all the Fibonacci numbers till f(n) and then add them up.
 
-Method 1 (O(n))
-Brute Force approach is pretty straight forward, find all the Fibonacci numbers till f(n) and then add them up.
-
-*/
-
-int calculateSum(int n) 
-{ 
-    if (n <= 0) 
-       return 0; 
-  
-    int fibo[n+1]; 
-    fibo[0] = 0, fibo[1] = 1; 
-
-    int sum = fibo[0] + fibo[1]; 
-  
-    for (int i=2; i<=n; i++) 
-    { 
-        fibo[i] = fibo[i-1]+fibo[i-2]; 
-        sum += fibo[i]; 
-    } 
-  
-    return sum; 
-} 
-  
-/*
-
-Method 2 (O(Log n))
-The idea is to find relationship between the sum of Fibonacci numbers and n’th Fibonacci number.
-
-F(i) refers to the i’th Fibonacci number.
-S(i) refers to sum of Fibonacci numbers till F(i),
-
-We can rewrite the relation F(n+1) = F(n) + F(n-1) as below
-F(n-1)    = F(n+1)  -  F(n)
-
-Similarly,
-F(n-2)    = F(n)    -  F(n-1)
-.          .           .
-.          .             .
-.          .             .
-F(0)      = F(2)    -  F(1)
--------------------------------
-Adding all the equations, on left side, we have
-F(0) + F(1) + … F(n-1) which is S(n-1).
-
-Therefore,
-S(n-1) = F(n+1) – F(1)
-S(n-1) = F(n+1) – 1
-S(n) = F(n+2) – 1 —-(1)
-
-In order to find S(n), simply calculate the (n+2)’th Fibonacci number and subtract 1 from the result
-
-*/
+**Method** 2 (O(Log n)) : The idea is to find relationship between the sum of Fibonacci numbers and n’th Fibonacci number.
 
 F(n) can be evaluated in O(log n) time Matrices
 
